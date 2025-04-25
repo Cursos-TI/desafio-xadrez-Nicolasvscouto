d37e23b08789f9a5ac7a765ebf31a8425d6b2a40
nicolasvscouto <nscouto66@gmail.com>
2025-04-25 11:42:43 -0300

    Adicionado nivel mestre do desafio

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+
+//funcoes recursivas para torre, rainha e bispo
+
+void RecursividadeTorre(int moviTorre){
+    if(moviTorre > 0){
+      printf("Direita\n");
+      RecursividadeTorre(moviTorre - 1);
+    }
+
+}
+
+void RecursividadeRainha(int moviRainha){
+   if(moviRainha > 0){
+    printf("ESQUERDA!\n");
+    RecursividadeRainha(moviRainha - 1);
+   }
+
+}
+
+void RecursividadeBispo(int moviBispo){
+  if(moviBispo > 0){
+    printf("CIMA, DIREITA!\n");
+    RecursividadeBispo(moviBispo - 1);
+  }
+
+}
+
 int main() {
     char Torre[10] = "Torre", Rainha[10] = "Rainha", Bispo[10] = "Bispo";
     int escolhaPeca;
@@ -16,52 +43,56 @@ int main() {
         scanf("%d", &escolhaPeca);
 
 
-        system("cls");
+       system("cls");
         switch(escolhaPeca){
             case 1:
               printf("Voce escolheu a Torre!\n");
-              for(int i = 1; i<= 5; i++){
-                printf("DIREITA!\n");
-              }
+              RecursividadeTorre(5);
               printf("Parabens, voce moveu a torre 5x para direita!\n");
               break;
 
             case 2:
              printf("Voce escolheu a Rainha!\n");
-             int rainhaMovimentacao = 1;
-             while(rainhaMovimentacao <= 8){
-                printf("ESQUERDA!\n");
-                rainhaMovimentacao++;
-             }
-             printf("Parabens, voce moveu a rainha 8x para esquerda! \n");
+             RecursividadeRainha(8);
+             printf("Parabens, voce moveu a rainha 8x para esquerda!\n");
              break;
 
             case 3:
              printf("Voce escolheu o bispo!\n");
-             int bispoMovimentacao = 1;
-             do{
-                printf("CIMA, DIREITA!\n");
-                bispoMovimentacao++;
-             }while(bispoMovimentacao <= 5);
-             printf("Parabens, voce moveu o bispo 5x para diagonal! \n");
+             //vamos utilizar um loop aninhado no bispo, mas a funcao recursiva tbm funciona
+             for(int i = 0; i < 5; i++){
+              for(int j = 0; j < i; j++){
+                printf("CIMA, DIREITA\n");
+              }
+             }
+             printf("Parabens, voce moveu o bispo na diagonal!\n");
              break;
-
+            
+        //aqui foi utilizado um loop aninhado paraa que, quando o usuario digite quantas vezes ele quer mover o cavalo, a quantidaade desejada seja o numero de vezes que o cavalo move
             case 4:
             printf("Voce Escolheu mover o cavalo!\n");
-             do{
-              for(int i = 0; i<2 ;i++){
-                printf("Baixo!\n");
+            int numeroMovi;
+            printf("Quantas vezes voce desejar mover o cavalo? (Pode mexer 1 vez para direita e 2 para cima)");
+            scanf("%d", &numeroMovi);
+             if(numeroMovi > 3){
+               printf("Voce nao pode mover o cavalo tanto assim!");
+             }else{
+              for(int lance = 0; lance < numeroMovi; lance++){
+                for(int i = 0; i < 2; i++){
+                 printf("Cima!");
+                }
+                for(int j = 0; j < 1; j++){
+                 printf("Direita!");
+                }
               }
-              printf("Esquerda!\n");
-             }while(j > 1);
-             printf("Voce moveu o cavalo 2x para baixo e uma vez para esquerda! \n");
+             }
+             printf("Parabens, voce moveu o cavalo duas vezes paraa cima e uma vez para a direita");
              break;
 
             case 5:
              printf("Saindo...");
              break;
         }
-
     }while(escolhaPeca != 5);
 
     return 0;
